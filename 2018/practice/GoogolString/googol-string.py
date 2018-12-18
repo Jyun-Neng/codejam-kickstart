a6c36435aaf3d1307263c9139f1bf8db76448526
Jyun-NengJi <jyunnengji@gmail.com>
2018-12-18 21:42:18 +0800

    Solve Googol string

---
+++ 2018/practice/GoogolString/googol-string.py
@@ -0,0 +1,18 @@
+def solve(k):
+    k_mod_8 = k % 8
+    if (k_mod_8 == 0) or (k_mod_8 == 1) or (k_mod_8 == 4):
+        return 0
+    if (k_mod_8 == 2) or (k_mod_8 == 5) or (k_mod_8 == 6):
+        return 1
+    if k_mod_8 == 3:
+        if (k // 8) % 2 == 0:
+            return 0
+        return 1
+    if k_mod_8 == 7:
+        k //= 8
+        return solve(k)
+
+t = int(input())
+for test in range(1, t + 1):
+    k = int(input())
+    print("Case #{}: {}".format(test, solve(k-1)))
